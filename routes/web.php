cf57c1bbb6eca36c1d6de0f2015ef16b67b6a153
Anton Maslinskiy <anton.maslinsky@gmail.com>
2021-08-03 20:28:35 +0300

    Выполнено дз к первому уроку.

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('info', function (){
+    echo "Информация о проекте";
+});
+
+Route::get('news', function (){
+    echo "Страница новостей";
+});
+
+
+
