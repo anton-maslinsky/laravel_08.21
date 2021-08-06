d692b28bfe50f49d6ee9a63ed81cf6bae3a151c5
Anton Maslinskiy <anton.maslinsky@gmail.com>
2021-08-06 20:11:12 +0300

    Домашнее задание к уроку 2. Добавил контроллеры: admin.CategoryController admin.NewsController NewsController Добавил массивы с данными о категориях и новостях в основной контроллер.

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
+//Controllers
+use App\Http\Controllers\NewsController;
+use App\Http\Controllers\Admin\CategoryController as AdminCategoryController;
+use App\Http\Controllers\Admin\NewsController as AdminNewsController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,17 +18,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
+Route::group(['prefix' => 'admin', 'as' => 'admin.'], function () {
+    Route::resource('categories', AdminCategoryController::class);
+    Route::resource('news', AdminNewsController::class);
 });
 
-Route::get('info', function (){
-    echo "Информация о проекте";
-});
 
-Route::get('news', function (){
-    echo "Страница новостей";
+Route::group(['prefix' => 'news'], function () {
+    Route::get('/', [NewsController::class, 'index'])
+        ->name('news');
+    Route::get('/show/{id}', [NewsController::class, 'show'])->where('id', '\d+')
+        ->name('news.show');
 });
-
 
 
